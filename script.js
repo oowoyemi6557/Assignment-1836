edbdf2f0d81a2dea0d28b9e17502de34905746ef
Oowoyemi <oowoyemi6557@conestogac.on.ca>
2023-08-13 21:43:56 -0400

    New commit Home

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
 
-menu.onclick = () =>{
-
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-
-}
+menu.onclick = () => {
+  menu.classList.toggle("fas fa-bars");
+  navbar.classList.toggle("active");
+};
 
 //navbar.onclick.toggle('active');
-window.onscroll = () =>{
-
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-
-    if(window.scrollY > 60){
-        document.querySelector('#scroll-top').classList.add('active');
-    }else{
-        document.querySelector('#scroll-top').classList.remove('active');
-    }
-}
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
