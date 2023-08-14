d317f0015c86d41a6fdcec0ac97ee358bff26a23
Oowoyemi <oowoyemi6557@conestogac.on.ca>
2023-08-13 23:01:37 -0400

    new change commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let menu = document.querySelector("#menu-bar");
 let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-  menu.classList.toggle("fas fa-times");
+  menu.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 };
 
@@ -10,4 +10,10 @@ menu.onclick = () => {
 window.onscroll = () => {
   menu.classList.remove("fa-times");
   navbar.classList.remove("active");
+
+  if (window.scrollY > 60) {
+    document.querySelector("#scroll-top").classList.add("active");
+  } else {
+    document.querySelector("#scroll-top").classList.remove("active");
+  }
 };
